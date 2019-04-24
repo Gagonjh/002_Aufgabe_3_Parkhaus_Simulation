acd189bb66df93323b4b4df87774c67e5de7c7e6
Gagonjh <49719775+Gagonjh@users.noreply.github.com>
2019-04-24 11:40:40 +0200

    test

--- src/Parkhaus_Servlet.java
+++ src/Parkhaus_Servlet.java
@@ -27,7 +27,7 @@ public class Parkhaus_Servlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Served at: hallo").append(request.getContextPath());
 	}
 
 	/**
